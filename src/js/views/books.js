22addfbe8eb54bd48bc698a6d7be06d56c6e4125
Kristina <cristina.chiticari@gmail.com>
2020-05-26 17:56:42 +0000

    states

--- src/js/views/books.js
+++ src/js/views/books.js
@@ -7,13 +7,21 @@ export const Books = props => {
 	const { store, actions } = useContext(Context);
 	const [numPages, setNumPages] = useState(null);
 	const [pageNumber, setPageNumber] = useState(1);
+	const [numPagess, setNumPagess] = useState(null);
+	const [pageNumberr, setPageNumberr] = useState(1);
 
 	function onDocumentLoadSuccess({ numPages }) {
 		setNumPages(numPages);
 	}
 
+	function onDocumentLoadSuccess1({ numPagess }) {
+		setNumPagess(numPagess);
+	}
 	function changePage(offset) {
 		setPageNumber(prevPageNumber => prevPageNumber + offset);
+	}
+	function changePage(offset) {
+		setPageNumberr(prevPageNumber => prevPageNumber + offset);
 	}
 
 	function previousPage() {
@@ -25,16 +33,13 @@ export const Books = props => {
 	}
 
 	return (
-		<div
-			className=" w-100 mx-auto"
-			style={{
-				backgroundImage: "url()"
-			}}>
+		<div className=" w-100 mx-auto">
 			<div className=" col ">
 				<hr style={{ border: "1px solid rgb(39, 39, 39)", marginTop: "3%" }} />
 				<h3 style={{ color: "grey", marginLeft: "2%", fontFamily: "Candal" }}>BOOKS //</h3>
 				<hr style={{ border: "1px solid rgb(39, 39, 39" }} />
 			</div>
+
 			<div className="row">
 				<div
 					className="col-5"
@@ -48,9 +53,6 @@ export const Books = props => {
 						onLoadSuccess={onDocumentLoadSuccess}>
 						<Page pageNumber={pageNumber} />
 					</Document>
-					{/* <p>
-					Page {pageNumber} of {numPages}
-                </p> */}
 					<div>
 						<p style={{ marginLeft: "4%" }}>
 							Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
@@ -69,7 +71,7 @@ export const Books = props => {
 				</div>
 
 				<div
-					className="col-5"
+					className="col-lg-5 col-12 "
 					style={{
 						border: "solid grey",
 						margin: "0% 0% 2% 2%",
@@ -77,35 +79,25 @@ export const Books = props => {
 					}}>
 					<Document
 						file="https://res.cloudinary.com/div5hqtbd/image/upload/v1590449935/salad_print_book_gbfv8b.pdf"
-						onLoadSuccess={onDocumentLoadSuccess}>
-						<Page pageNumber={pageNumber} />
+						onLoadSuccess={onDocumentLoadSuccess1}>
+						<Page pageNumber={pageNumberr} />
 					</Document>
 					<div>
 						<p style={{ marginLeft: "4%" }}>
-							Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+							Page {pageNumberr || (numPagess ? 1 : "--")} of {numPagess || "--"}
 						</p>
 						<button
 							style={{ marginLeft: "4%" }}
 							type="button"
-							disabled={pageNumber <= 1}
+							disabled={pageNumberr <= 1}
 							onClick={previousPage}>
 							Previous
 						</button>
-						<button type="button" disabled={pageNumber >= numPages} onClick={nextPage}>
+						<button type="button" disabled={pageNumberr >= numPagess} onClick={nextPage}>
 							Next
 						</button>
 					</div>
 				</div>
-
-				{/* <div
-				className="row"
-				style={{
-					marginBottom: "5%"
-				}}>
-				{store.favorites.map((fav, index) => (
-					<Card key={index} name={store.recipe[fav].name} />
-				))}
-            </div> */}
 			</div>
 		</div>
 	);
